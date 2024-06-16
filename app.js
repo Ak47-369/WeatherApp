28ab7fd0dad044e385f8158dfd0d6f78bb667902
Amit Yadav <akydv2627@gmail.com>
2024-06-16 16:05:38 +0530

    Handled 404 Error Show and 404 image

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.post("/",function(request,response){
       openWeatherMap: "f16726fe714ecaae4b0de2153eee210a",
       pirateWeather: "Q9Kj2H5UbMqb65FW25Y32EUNOOGT0fxf",
       weatherBit: "d359e8eb4b5f483ba435f2a0c95f2b64",
-      tommorow: "LwRLVEBT7gJEnOx48F2mauGzbIXf6Vwl"
+      tomorrow: "LwRLVEBT7gJEnOx48F2mauGzbIXf6Vwl"
     };
   
     const weatherData = {};
@@ -38,12 +38,12 @@ app.post("/",function(request,response){
         lat_long = data[0].lat + "," + data[0].lon;
         console.log(lat_long);
         
-        // To ensure that all API's called after geoCode
+        // To ensure that all API's called after geoCode API
         const apiUrls = {
           openWeatherMap: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKeys.openWeatherMap}&units=metric`,
           weatherBit: `https://api.weatherbit.io/v2.0/current?city=${cityName}&key=${APIKeys.weatherBit}`,
           pirateWeather: `https://api.pirateweather.net/forecast/${APIKeys.pirateWeather}/${lat_long}?&units=si`,
-          tommorow: `https://api.tomorrow.io/v4/weather/forecast?location=${cityName}&apikey=${APIKeys.tommorow}`
+          tomorrow: `https://api.tomorrow.io/v4/weather/forecast?location=${cityName}&apikey=${APIKeys.tomorrow}`
         };
 
         // Now call all Weather API's
@@ -52,8 +52,12 @@ app.post("/",function(request,response){
             // const res = await https.get(apiUrls[api]);
             let res = await fetch(apiUrls[api]);
             let data = await res.json(); // Parse json into js object
+            console.log(res.status);
+            // Using spread operator ...
+            data = { ...data, statusCode: res.status }; // Add statusCode property to data object
             console.log(apiUrls[api]);
             console.log(data);
+            // console.log(data.sta)
             weatherData[api] = data;
           }
         })();
@@ -98,34 +102,47 @@ app.post("/",function(request,response){
 
           <div class="weather-box">
             <h2>OpenWeatherMap</h2>
-            <img width = "72px" height = "96px" src =https://openweathermap.org/img/wn/${weatherData.openWeatherMap.weather[0].icon}@2x.png>
-            <p>Temperature: ${weatherData.openWeatherMap.main.temp}°C</p>
-            <p>Weather: ${weatherData.openWeatherMap.weather[0].main}</p>
-            <p>Description: ${weatherData.openWeatherMap.weather[0].description}</p>
+            ${weatherData.openWeatherMap.statusCode === 200 ? 
+              `<img width="72px" height="96px" src="https://openweathermap.org/img/wn/${weatherData.openWeatherMap.weather[0].icon}@2x.png>
+              <p>Temperature: ${weatherData.openWeatherMap.main.temp}°C</p>
+              <p>Weather: ${weatherData.openWeatherMap.weather[0].main}</p>
+              <p>Description: ${weatherData.openWeatherMap.weather[0].description}</p>` 
+              : 
+              `<img src="./assets/error-image.png" alt="Api Request Unsuccessful">`
+            }
           </div>
+
         
           <div class="weather-box">
             <h2>WeatherBit</h2>
-            <img width = "72px" height = "96px"  src=https://www.weatherbit.io/static/img/icons/${weatherData.weatherBit.data[0].weather.icon}.png>
+            ${weatherData.weatherBit.statusCode === 200 ?
+            `<img width = "72px" height = "96px"  src=https://www.weatherbit.io/static/img/icons/${weatherData.weatherBit.data[0].weather.icon}.png>
             <p>Temperature: ${weatherData.weatherBit.data[0].temp}°C</p>
             <p>Weather: ${weatherData.weatherBit.data[0].weather.description}</p>
-            <p>AQI: ${weatherData.weatherBit.data[0].aqi}</p>
+            <p>AQI: ${weatherData.weatherBit.data[0].aqi}</p>` : `<img src = "./assets/error-image.png" alt = "Api Request Unsuccessful ">`
+            }   
           </div>
 
           <div class = "weather-box">
-            <h2> Tommorrow API</h2>
-            <img  width = "72px" height = "96px" src ="./assets/${weatherData.tommorow.timelines.minutely[0].values.weatherCode}.png">
-            <p>Temperature: ${weatherData.tommorow.timelines.minutely[0].values.temperature}°C</p>
-            <p>Humidity: ${weatherData.tommorow.timelines.minutely[0].values.humidity}</p>
-            <p>WindSpeed: ${weatherData.tommorow.timelines.minutely[0].values.windSpeed}</p>
+            <h2> Tomorrow API</h2>
+            ${weatherData.tomorrow.statusCode === 200 ?
+            `<img  width = "72px" height = "96px" src ="./assets/${weatherData.tomorrow.timelines.minutely[0].values.weatherCode}.png">
+            <p>Temperature: ${weatherData.tomorrow.timelines.minutely[0].values.temperature}°C</p>
+            <p>Humidity: ${weatherData.tomorrow.timelines.minutely[0].values.humidity}</p>
+            <p>WindSpeed: ${weatherData.tomorrow.timelines.minutely[0].values.windSpeed}</p>` 
+            : `<img src = "./assets/error-image.png" alt = "Api Request Unsuccessful">`
+            }   
           </div>
 
             <div class="weather-box">
             <h2>Pirate Weather</h2>
-            <img  width = "72px" height = "96px" src ="./assets/${weatherData.pirateWeather.currently.icon}.png">
+            ${weatherData.pirateWeather.statusCode === 200 ?
+            `<img  width = "72px" height = "96px" src ="./assets/${weatherData.pirateWeather.currently.icon}.png">
             <p>Temperature: ${weatherData.pirateWeather.currently.temperature}°C</p>
             <p>Weather: ${weatherData.pirateWeather.currently.summary}</p>
-            <p>Description: ${weatherData.pirateWeather.currently.windSpeed}</p>
+            <p>Description: ${weatherData.pirateWeather.currently.windSpeed}</p>` :
+              `<img src = "./assets/error-image.png" alt = "Api Request Unsuccessful">`
+            }
           </div>
 
         </body>
@@ -134,7 +151,7 @@ app.post("/",function(request,response){
     
     response.setHeader("Content-Type", "text/html");
     response.send(html);
-    },5000);
+    },6000);
 });
 
 app.listen(process.env.PORT || 3000, function(){
